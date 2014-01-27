2298c1f4f1fc921a412e06eabe9ebf03ffc053f6
Eugene Petkevich <eugene@thinkpad121e.comp>
2014-01-27 17:41:08 +0100

    before total refactoring

--- main.cpp
+++ main.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <bitset>
+#include <vector>
+#include <fstream>
 using namespace std;
 
 const int MATRIX_SIZE = 2;
 
 typedef bitset<MATRIX_SIZE*MATRIX_SIZE*MATRIX_SIZE*MATRIX_SIZE> Multiplication_vector;
 typedef bitset<MATRIX_SIZE*MATRIX_SIZE> Matrix_vector;
+typedef bitset<MATRIX_SIZE*MATRIX_SIZE*MATRIX_SIZE-1> Linear_vector;
 
 // for positive b
 int power(int a, int b);
+// finds if there is at least one solution for system of linear equations in a vector form
+bool gauss_solve(vector<Multiplication_vector> a, Multiplication_vector b);
+// finds if there is at least one solution for system of linear equations in a vector form
+bool binary_solve(vector<Multiplication_vector> a, Multiplication_vector b);
 
 class Matrix_Multiplication_Checker
 {
@@ -19,6 +26,8 @@ class Matrix_Multiplication_Checker
     Multiplication_vector * m_vectors;
     int m_count;
     int m_length;
+    int f_count;
+    vector<int> good_vectors;
 
     Matrix_Multiplication_Checker(int length);
     ~Matrix_Multiplication_Checker();
@@ -27,6 +36,8 @@ class Matrix_Multiplication_Checker
     int get_vector_index(int ai, int aj, int bi, int bj);
     // returns linear index in a bit vector by 2 indexes in matrices
     int get_vector_index(int a, int b);
+    // returns 4 indices from index
+    void decode_indices_from_index(int index, int & ai, int & aj, int & bi, int & bj);
     // returns linear index of an elementt in a matrix
     int get_element_index(int i, int j);
     // returns index of vertar in m
@@ -35,8 +46,12 @@ class Matrix_Multiplication_Checker
     void calculate_r_vectors();
     // write different multiplication vectors to array
     void calculate_m_vectors();
-    // outputs vector to memory
+    // outputs vector to screen
     void output_vector(Multiplication_vector v);
+    // outputs vector to screen in letters
+    void output_vector_text(Multiplication_vector v);
+    // finds if there are good vectors (what we want to find)
+    bool check_for_good_vectors();
 };
 
 int main()
@@ -45,12 +60,23 @@ int main()
 
     Matrix_Multiplication_Checker * checker = new Matrix_Multiplication_Checker(MATRIX_SIZE);
 
+    /*
     for (int i = 0; i < checker->length; ++i)
         for (int j = 0; j < checker->length; ++j)
         {
-            checker->output_vector(checker->r_vectors[checker->get_element_index(i, j)]);
+            checker->output_vector_text(checker->r_vectors[checker->get_element_index(i, j)]);
             cout << "\n";
         }
+
+    for (int i = 0; i < checker->m_count; ++i)
+    {
+        cout << i << " : ";
+        checker->output_vector_text(checker->m_vectors[i]);
+        cout << "\n";
+    }
+    */
+
+    checker->check_for_good_vectors();
 
     delete checker;
 
@@ -68,6 +94,7 @@ Matrix_Multiplication_Checker::Matrix_Multiplication_Checker(int length)
     m_count = m_length*m_length;
     m_vectors = new Multiplication_vector[m_count];
     calculate_m_vectors();
+    f_count = length*length*length-1;
 }
 
 Matrix_Multiplication_Checker::~Matrix_Multiplication_Checker()
@@ -86,6 +113,18 @@ int Matrix_Multiplication_Checker::get_vector_index(int ai, int aj, int bi, int 
     result *= length;
     result += bj;
     return result;
+}
+
+void Matrix_Multiplication_Checker::decode_indices_from_index(int index, int & ai, int & aj, int & bi, int & bj)
+{
+    bj = index % length;
+    index /= length;
+    bi = index % length;;
+    index /= length;
+    aj = index % length;
+    index /= length;
+    ai = index;
+    return;
 }
 
 int Matrix_Multiplication_Checker::get_vector_index(int a, int b)
@@ -137,10 +176,39 @@ void Matrix_Multiplication_Checker::calculate_m_vectors()
                         if (bv[l])
                             m_vectors[index].set(get_vector_index(k, l));
                 }
-            cout << index << " : ";
-            output_vector(m_vectors[index]);
-            cout << "\n";
         }
+}
+
+bool Matrix_Multiplication_Checker::check_for_good_vectors()
+{
+    good_vectors.clear();
+    // for 2x2 matrix multiplication
+    for (int c1 = 0; c1 < m_count-2; ++c1)
+		for (int c2 = c1+1; c2 < m_count-1; ++c2)
+			for (int c3 = c2+1; c3 < m_count; ++c3)
+			{
+				vector<Multiplication_vector> n_vectors;
+				// 128  165  9  109  37  62  146
+				n_vectors.push_back(m_vectors[c1]);
+				n_vectors.push_back(m_vectors[c2]);
+				n_vectors.push_back(m_vectors[c3]);
+				for (int i = 0; i < element_count; ++i)
+					n_vectors.push_back(r_vectors[i]);
+				int n_index = 0;
+				int good_count = 0;
+				for (int i = n_index; i < m_count; ++i)
+				{
+					if (gauss_solve(n_vectors, m_vectors[i]))
+					{
+						++good_count;
+						//cout << "number " << i << "\n";
+						//output_vector_text(m_vectors[i]);
+						//cout << "\n";
+					}
+				}
+				if (good_count >= f_count)
+					cout << "Found good vectors: " << c1 << " " << c2 << " " << c3 << "(gc = " << good_count << ")\n";
+			}
 }
 
 void Matrix_Multiplication_Checker::output_vector(Multiplication_vector v)
@@ -151,6 +219,18 @@ void Matrix_Multiplication_Checker::output_vector(Multiplication_vector v)
     }
 }
 
+void Matrix_Multiplication_Checker::output_vector_text(Multiplication_vector v)
+{
+    for (int i = 0; i < v.size(); ++i)
+    {
+        if (v[i])
+        {
+            int ai, aj, bi, bj;
+            decode_indices_from_index(i, ai, aj, bi, bj);
+            cout << "A" << ai+1 << aj+1 << "B" << bi+1 << bj+1 << " ";
+        }
+    }
+}
 
 int power(int a, int b)
 {
@@ -158,5 +238,76 @@ int power(int a, int b)
     while (--b)
         result *= a;
     return result;
+}
+
+bool gauss_solve(vector<Multiplication_vector> a, Multiplication_vector b)
+{
+    for (int i = 0; i < a.size(); ++i) // for all columns (7)
+    {
+        // first we exchange rows if needed, so that a[i][i] = 1
+        if (!a[i][i])
+        {
+            int k = i+1;
+            while (!a[i][k])
+                ++k;
+            bool tmp = b[k];
+            b[k] = b[i];
+            b[i] = tmp;
+            for (int l = i; l < a.size(); ++l)
+            {
+                tmp = a[l][k];
+                a[l][k] = a[l][i];
+                a[l][i] = tmp;
+            }
+        }
+        // second, we make all coefficients under a[i][i] equal to 0
+        for (int j = i+1; j < b.size(); ++j) // for all rows (16)
+        {
+            if (a[i][j])
+            {
+                for (int k = i; k < a.size(); ++k)
+                {
+                    a[k][j] = (a[k][j] != a[k][i]);
+                }
+                b[j] = (b[j] != b[i]);
+            }
+        }
+    }
+    // we chech all i from 7 to 15 for zero coefficients
+    for (int i = a.size(); i < b.size(); ++i)
+    {
+        if (b[i])
+            return false;
+    }
+    return true;
+}
+
+bool binary_solve(vector<Multiplication_vector> a, Multiplication_vector b)
+{
+    uint_least64_t counter;
+    uint_least64_t limit = power(2,MATRIX_SIZE*MATRIX_SIZE*MATRIX_SIZE-1);
+    for (counter = 1; counter < limit; ++counter)
+    {
+        Linear_vector x(counter);
+        Multiplication_vector r(0);
+        bool is_good = true;
+        // we multiply a by x
+        for (int i = 0; i < b.size(); ++i)
+        {
+            for (int j = 0; j < a.size(); ++j)
+            {
+                if (x[j])
+                    r[i] = (r[i] != a[j][i]);
+            }
+            if (r[i] != b[i])
+            {
+                is_good = false;
+                break;
+            }
+        }
+        if (is_good)
+            return true;
+    }
+    return false;
 }
 
