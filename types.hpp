95372dd7ec7d25ba07e57841b1d69c9b15880c49
Eugene Petkevich <eugene@thinkpad121e.comp>
2014-02-20 10:49:02 +0100

    works well, with gauss presolve (update)

---
+++ types.hpp
@@ -0,0 +1,16 @@
+#ifndef TYPES_HPP_INCLUDED
+#define TYPES_HPP_INCLUDED
+
+#include <bitset>
+
+using namespace std;
+
+typedef bitset<16> Bitvector_16;
+typedef bitset<4> Bitvector_4;
+typedef bitset<7> Bitvector_7;
+
+typedef bitset<512> Bitvector_512;
+typedef bitset<8> Bitvector_8;
+typedef bitset<15> Bitvector_15;
+
+#endif // TYPES_HPP_INCLUDED
