8b925f01005cfd977549158adb8053001574b6ec
Eugene Petkevich <eugene@thinkpad121e.comp>
2014-03-10 23:21:10 +0100

    new files added

--- main.cpp
+++ main.cpp
@@ -11,26 +11,7 @@ using namespace std;
 
 int main()
 {
-    // generate all possiple multiplications (> 200 vectors)
-
     Matrix_Multiplication_Checker * checker = new Matrix_Multiplication_Checker(2);
-
-    /*
-    for (int i = 0; i < checker->length; ++i)
-        for (int j = 0; j < checker->length; ++j)
-        {
-            checker->output_vector_text(checker->r_vectors[checker->get_element_index(i, j)]);
-            cout << "\n";
-        }
-
-    for (int i = 0; i < checker->m_count; ++i)
-    {
-        cout << i << " : ";
-        checker->output_vector_text(checker->m_vectors[i]);
-        cout << "\n";
-    }
-    */
-
     checker->check_for_good_vectors();
 
     delete checker;
