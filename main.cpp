b35fb3b4b880e57c46d1ba4718dfa6184523cbb7
genio <genio@thinkpad.sl510.comp>
2015-02-25 17:17:21 +0100

    Add command line arguments.

--- main.cpp
+++ main.cpp
@@ -1,25 +1,32 @@
 //#define output2
 //#define output1
+//#define statistics
+
+#include <iostream>
 
 #include "slae.hpp"
 #include "mmchecker.hpp"
 
-int main()
+int main(int argc , char * argv)
 {
-    Matrix_Multiplication_Checker<2, 2, 16, 4> * checker = new Matrix_Multiplication_Checker<2, 2, 16, 4>;
-    //Matrix_Multiplication_Checker<2, 3, 512, 8> * checker = new Matrix_Multiplication_Checker<2, 3, 512, 8>;
+    if (argc > 1)
+    {
+        std::cout << "no arguments supported yet!";
+    }
+    else
+    {
+        std::cout << "Cube Product Checker (version pre-alpha)" << std::endl << std::endl
+                  << "Usage:  ..." << std::endl << std::endl
+                  << "Starting default action...";
 
-    checker->start_statistics("stats2d.txt");
-
-    checker->check_for_good_vectors();
-    //checker->check_for_good_vectors_randomized();
-
-    //checker->save_random_samples(1000000, "2test1.txt");
-    //checker->read_samples_and_check("2test1.txt", "2test1-res.txt");
-
-	checker->end_statistics();
-
-    delete checker;
+        Matrix_Multiplication_Checker<2, 2, 16, 4> * checker = new Matrix_Multiplication_Checker<2, 2, 16, 4>;
+        //Matrix_Multiplication_Checker<2, 3, 512, 8> * checker = new Matrix_Multiplication_Checker<2, 3, 512, 8>;
+        checker->check_for_good_vectors();
+        //checker->check_for_good_vectors_randomized();
+        //checker->save_random_samples(1000000, "2test1.txt");
+        //checker->read_samples_and_check("2test1.txt", "2test1-res.txt");
+        delete checker;
+    }
 
     return 0;
 }
